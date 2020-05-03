ce0187f9687efaaaff1ab09efb70c6c4a612345f
Dede Irwanto <52788483+bukanprogrammer@users.noreply.github.com>
2020-05-03 12:01:56 +0800

    Update blogger-menu.js

--- blogger-menu.js
+++ blogger-menu.js
@@ -4,8 +4,10 @@ $(function() {
   console.log(menu.length);
 
   if(menu) {
-    menu.parent().addClass('dropdown');
-    menu.addClass('dropdown-toggle');
-    menu.attr({'id':"dropdown", 'data-toggle':"dropdown", 'aria-haspopup':true, 'aria-expanded':false});
+    for(var i=1; i <= menu.length; i++) {
+      menu.parent().addClass('dropdown');
+      menu.addClass('dropdown-toggle');
+      menu.attr({'id':"dropdown". "+i+" .", 'data-toggle':"dropdown", 'aria-haspopup':true, 'aria-expanded':false});
+    }
   }	
 });
